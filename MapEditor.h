21e26c961d0385963b7f6565143a27036f816fd6
MostafaBJN <Mostafa.BJN.80@gmail.com>
2020-01-31 00:34:14 +0330

    .h of Map Editor

---
+++ MapEditor.h
@@ -0,0 +1,14 @@
+#ifndef MAPEDITOR_H_INCLUDED
+#define MAPEDITOR_H_INCLUDED
+
+void new_map();
+
+void random_map();
+
+void editor();
+
+void view();
+
+void Map_Editor();
+
+#endif // MAPEDITOR_H_INCLUDED
